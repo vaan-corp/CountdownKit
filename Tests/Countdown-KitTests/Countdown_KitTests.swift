0b67ba1a868e960f50452bd39d32b847736ed0ee
Asif <sheikasifofficial@gmail.com>
2023-01-30 12:01:43 +0530

    Add source files and fix indentation

---
+++ Tests/Countdown-KitTests/Countdown_KitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Countdown_Kit
+
+final class Countdown_KitTests: XCTestCase {
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(Countdown_Kit().text, "Hello, World!")
+  }
+}
